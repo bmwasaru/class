42f0b7517f7f31869f7e4bd789b542e0f0ef6990
Britone Mwasaru <bmwasaru@gmail.com>
2018-07-23 09:33:40 +0300

    additions

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -33,7 +33,7 @@ def init_db():
 @app.cli.command('initdb')
 def initdb_command():
 	init_db()
-	print(("Initialized the database"))
+	print("Initialized the database")
 
 
 def get_db():
@@ -57,7 +57,7 @@ def show_entries():
 	return render_template('show_entries.html', entries=entries)
 
 
-@app.route('/add')
+@app.route('/add', methods=["POST"])
 def add_entry():
 	if not session.get('logged_in'):
 		abort(401)
@@ -72,10 +72,10 @@ def add_entry():
 @app.route('/login', methods=["GET","POST"])
 def login():
 	error = None
-	if request.method = "POST":
-		if request.form["username"] != app.config["username"]:
+	if request.method == "POST":
+		if request.form["username"] != app.config["USERNAME"]:
 			error = "Invalid username"
-		elif request.form["password"] != app.config["password"]:
+		elif request.form["password"] != app.config["PASSWORD"]:
 			error = "Invalid password"
 		else:
 			session["logged_in"] = True
--- flaskr/setup.py
+++ flaskr/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
 	name="flaskr",
+	author="Britone Mwasaru",
 	packages=["flaskr"],
 	include_package_data=True,
 	install_requires=[
